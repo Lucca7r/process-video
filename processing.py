c1954c8052d6ce5990d57a3811ca46154d06f8ec
luanMedrado8 <medradoluan6@gmail.com>
2024-05-29 14:05:37 -0300

    Aplicando novos filtros

--- processing.py
+++ processing.py
@@ -10,11 +10,27 @@ def format_time(seconds):
     seconds %= 60
     return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
-def process_quadro(frame):
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return gray_frame
+def aplicar_sepia(frame):
+    sepia_filter = cv2.transform(frame, cv2.COLOR_BGR2BGRA)
+    sepia_filter[:, :, 0] = 0.272 * frame[:, :, 2] + 0.534 * frame[:, :, 1] + 0.131 * frame[:, :, 0]
+    sepia_filter[:, :, 1] = 0.349 * frame[:, :, 2] + 0.686 * frame[:, :, 1] + 0.168 * frame[:, :, 0]
+    sepia_filter[:, :, 2] = 0.393 * frame[:, :, 2] + 0.769 * frame[:, :, 1] + 0.189 * frame[:, :, 0]
+    return sepia_filter
 
-def process_video(input_video_path, output_video_path, batch_size, num_threads):
+def aplicar_negativo(frame):
+    return cv2.bitwise_not(frame)
+
+def process_quadro(frame, filter_type):
+    if filter_type == 'pb':
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    elif filter_type == 'sepia':
+        return aplicar_sepia(frame)
+    elif filter_type == 'negativo':
+        return aplicar_negativo(frame)
+    else:
+        raise ValueError("Tipo inválido de filtro. Use 'pb', 'sepia', ou 'negativo'.")
+
+def process_video(input_video_path, output_video_path, batch_size, num_threads, filter_type):
     inicial = time.time()
 
     video = cv2.VideoCapture(input_video_path)
@@ -23,8 +39,9 @@ def process_video(input_video_path, output_video_path, batch_size, num_threads):
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = video.get(cv2.CAP_PROP_FPS)
 
+    is_color = filter_type != 'gray'
     fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height), isColor=False)
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height), isColor=is_color)
 
     frame_queue = queue.Queue(maxsize=2000)
     output_queue = queue.PriorityQueue(maxsize=2000)
@@ -51,7 +68,7 @@ def process_video(input_video_path, output_video_path, batch_size, num_threads):
                 frames.append((frame_index, frame))
             if not frames:
                 break
-            processed_frames = [(frame_index, process_quadro(frame)) for frame_index, frame in frames]
+            processed_frames = [(frame_index, process_quadro(frame, filter_type)) for frame_index, frame in frames]
             for processed_frame in processed_frames:
                 output_queue.put(processed_frame)
         output_queue.put((-1, None))
@@ -77,4 +94,5 @@ def process_video(input_video_path, output_video_path, batch_size, num_threads):
     final = time.time()
     print("Tempo de processamento: ", format_time(final - inicial))
 
-process_video('./videos/Resident_Evil_2.mp4', './out/output1.mp4', 100, 2)
+
+process_video('./videos/trailerHomemAranha.mp4', './out/output1.mp4', 100, 2, 'negativo')
